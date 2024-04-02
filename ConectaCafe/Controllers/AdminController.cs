cd740f6d385bff4791b9f9a1d52d2fd1ca88af94
WellisLopesJr-Tech <wellislopesjrofficial@gmail.com>
2024-04-02 16:11:52 -0300

    Categorias

---
+++ ConectaCafe/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConectaCafe.Controllers;
+
+public class AdminController : Controller
+{
+    private readonly ILogger<AdminController> _logger;
+
+    public AdminController(ILogger<AdminController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View("Error!");
+    }
+}
